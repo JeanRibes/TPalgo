163ad74d12dd4e90cbdc7aab19d6fa42d55ddff0
Jean Ribes <jean.ribes@insa-lyon.fr>
2017-12-20 15:31:30 +0000

    exo fait

--- TP4 - Méthodes/TableauMethode.java
+++ TP4 - Méthodes/TableauMethode.java
@@ -19,8 +19,28 @@ public class TableauMethode {
         }
         int indiceMin=0;
         for (int i=0; i<t.length; i++) {
-            if(min = t[i]) { indiceMin=i;}
-
+            if(min == t[i]) { indiceMin=i;}
         }
+        return indiceMin;
+    }
+    public static double moyenne(int[] t) {
+        int n = t.length;
+        int somme = 0;
+        for(int i=0; i<t.length; i++) {
+            somme += t[i];
+        }
+        return somme/n;
+    }
+    public static double ecartType(int[] t, double moyenne) {
+        int somme = 0;
+        for(int i=0; i<t.length; i++) {
+            somme += t[i];
+        }
+        for (int i=0;i<t.length;i++) {
+            somme+=Math.pow(t[i]-moyenne, 2);
+        }
+        /*double ecartType = Math.sqrt(somme/t.length);
+        return ecartType;*/
+        return Math.sqrt(somme/t.length);
     }
 }
